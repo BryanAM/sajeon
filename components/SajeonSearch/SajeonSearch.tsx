9486178b95124a208488d788eae474dc2290cc22
Bryan Aument <bryaument@gmail.com>
2023-11-24 13:33:35 -0500

    added some basic components

--- components/SajeonSearch/SajeonSearch.tsx
+++ components/SajeonSearch/SajeonSearch.tsx
@@ -1,40 +1,33 @@
 import React from "react";
-import { Button } from "@/components/ui/button"
-
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 import { MagnifyingGlassIcon, ThickArrowUpIcon } from "@radix-ui/react-icons";
 
 function SajeonSearch() {
   return (
     <form className="flex flex-row items-end justify-center gap-2">
-      <Button>Sajeon Search</Button>
-      {/* <label className="flex max-w-md grow flex-col gap-2 text-center">
-        <Text as="label" size="8">
-          Sajeon
-        </Text>
-        <TextFieldRoot>
-          <TextFieldSlot className="md:hidden">
-            <IconButton size="1">
-              <MagnifyingGlassIcon height="12" width="12" />
-            </IconButton>
-          </TextFieldSlot>
-          <TextFieldSlot className="hidden md:flex">
-            <MagnifyingGlassIcon height="16" width="16" />
-          </TextFieldSlot>
-          <TextFieldInput
-            type="search"
-            enterKeyHint="search"
-            maxLength={200}
-            autoCapitalize="off"
-            autoComplete="off"
-            autoCorrect="off"
-            size="3"
-          />
-        </TextFieldRoot>
+      <label className="flex max-w-md grow flex-col gap-2 text-center">
+        Sajeon
+        <Button className="md:hidden">
+          <MagnifyingGlassIcon height="12" width="12" />
+        </Button>
+        <MagnifyingGlassIcon
+          className="hidden md:flex"
+          height="16"
+          width="16"
+        />
+        <Input
+          type="search"
+          enterKeyHint="search"
+          maxLength={200}
+          autoCapitalize="off"
+          autoComplete="off"
+          autoCorrect="off"
+          size={3}
+        />
       </label>
-      <Button className="hidden md:block" size="3">
-        Sajeon Search
-      </Button> */}
+      <Button className="hidden md:block">Sajeon Search</Button>
     </form>
   );
 }
