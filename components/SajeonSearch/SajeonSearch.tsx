c1266ef54939934a05664953be2238eeadc9277c
Bryan Aument <bryaument@gmail.com>
2023-11-24 17:21:50 -0500

    updated text label font size and color

--- components/SajeonSearch/SajeonSearch.tsx
+++ components/SajeonSearch/SajeonSearch.tsx
@@ -6,25 +6,35 @@ import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
 function SajeonSearch() {
   return (
     <form className="m-auto flex max-w-xl flex-col justify-center gap-2 text-center">
-      <label>Sajeon</label>
-      <div className="random-boy flex rounded-md border border-input bg-background px-1 py-1 ring-offset-background focus-within:ring-ring focus-within:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50 focus-within:[&:has(input:focus)]:focus-within:ring-2 ">
-        <Button className="mr-1 md:hidden">
-          <MagnifyingGlassIcon height="16" width="16" />
+      <label
+        htmlFor="sajeon-search-input"
+        className="bg-gradient-to-t from-neutral-900 to-neutral-700 bg-clip-text text-4xl font-extrabold text-transparent"
+      >
+        Sajeon
+      </label>
+      <div className="flex items-center gap-2">
+        <div className="flex grow rounded-md border border-input bg-background px-1 py-1 ring-offset-background focus-within:ring-ring focus-within:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50 focus-within:[&:has(input:focus)]:focus-within:ring-2 ">
+          <Button className="mr-1 md:hidden">
+            <MagnifyingGlassIcon height="16" width="16" />
+          </Button>
+          <span className="mr-1 hidden h-10 w-10 items-center justify-center md:flex">
+            <MagnifyingGlassIcon height="16" width="16" />
+          </span>
+          <Input
+            type="search"
+            variant="naked"
+            enterKeyHint="search"
+            maxLength={200}
+            autoCapitalize="off"
+            autoComplete="off"
+            autoCorrect="off"
+            name="sajeon-search-input"
+            id="sajeon-search-input"
+          />
+        </div>
+        <Button className="mr-1 hidden md:flex" size="lg">
+          Search
         </Button>
-        <span className="mr-1 flex hidden h-10 w-10 items-center justify-center md:flex">
-          <MagnifyingGlassIcon height="16" width="16" />
-        </span>
-
-        <Input
-          type="search"
-          variant="naked"
-          enterKeyHint="search"
-          maxLength={200}
-          autoCapitalize="off"
-          autoComplete="off"
-          autoCorrect="off"
-          size={3}
-        />
       </div>
     </form>
   );
