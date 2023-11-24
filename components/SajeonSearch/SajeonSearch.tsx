63f8465cd093a72014c60ebf41ab11fdb5f1261c
Bryan Aument <bryaument@gmail.com>
2023-11-23 20:17:21 -0500

    layout changes to default page, added sajeon search component form

--- components/SajeonSearch/SajeonSearch.tsx
+++ components/SajeonSearch/SajeonSearch.tsx
@@ -3,16 +3,43 @@ import {
   Text,
   Button,
   TextFieldInput,
+  TextFieldRoot,
+  TextFieldSlot,
+  IconButton,
 } from "@radix-ui/themes";
+
+import { MagnifyingGlassIcon, ThickArrowUpIcon } from "@radix-ui/react-icons";
 
 function SajeonSearch() {
   return (
-    <form>
-      <label className="flex flex-col">
-        <Text as="label">Sajeon</Text>
-        <TextFieldInput />
+    <form className="flex flex-row items-end justify-center gap-2">
+      <label className="flex max-w-md grow flex-col gap-2 text-center">
+        <Text as="label" size="8">
+          Sajeon
+        </Text>
+        <TextFieldRoot>
+          <TextFieldSlot className="md:hidden">
+            <IconButton size="1">
+              <MagnifyingGlassIcon height="12" width="12" />
+            </IconButton>
+          </TextFieldSlot>
+          <TextFieldSlot className="hidden md:flex">
+            <MagnifyingGlassIcon height="16" width="16" />
+          </TextFieldSlot>
+          <TextFieldInput
+            type="search"
+            enterKeyHint="search"
+            maxLength={200}
+            autoCapitalize="off"
+            autoComplete="off"
+            autoCorrect="off"
+            size="3"
+          />
+        </TextFieldRoot>
       </label>
-      <Button>Sajeon Search</Button>
+      <Button className="hidden md:block" size="3">
+        Sajeon Search
+      </Button>
     </form>
   );
 }
