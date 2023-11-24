a1f9b58488364515f0aefa80277cc9d27790f002
Bryan Aument <bryaument@gmail.com>
2023-11-24 18:30:47 -0500

    made title component for sajeon

---
+++ components/SajeonTitle/SajeonTitle.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SajeonTitle() {
+  return (
+    <div className="text-shadow bg-gradient-to-t from-neutral-900 to-neutral-700 bg-clip-text pb-3 text-8xl font-extrabold text-transparent md:text-9xl">
+      Sajeon
+    </div>
+  );
+}
+
+export default SajeonTitle;
