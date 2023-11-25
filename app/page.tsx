96c6cfdfbb78aac8730ba2fc7465d575ac4d9ccf
Bryan Aument <bryaument@gmail.com>
2023-11-25 13:16:35 -0500

    added shadcn dropdown, theme provider, sajeon navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
 'use client';
-import SajeonNavigation from "@/components/SajeonNavigation/SajeonNavigation";
 import SajeonSearch from "@/components/SajeonSearch/SajeonSearch";
 
 export default function Home() {
   return (
     <main className="min-h-screen">
-      <SajeonNavigation/>
+    
       <SajeonSearch />
     </main>
   )
--- components/SajeonNavigation/SajeonNavigation.tsx
+++ components/SajeonNavigation/SajeonNavigation.tsx
@@ -1,31 +1,40 @@
 import React from "react";
 import Link from "next/link";
+import { SajeonThemeSelector } from "../SajeonThemeSelector/SajeonThemeSelector";
 
 function SajeonNavigation() {
   return (
-    <div>
-      <nav
-        aria-label="Main"
+    <nav
+      aria-label="Main"
+      data-orientation="horizontal"
+      dir="ltr"
+      className="fixed left-0 right-0 top-0 bg-background/95  pb-2 pl-6 pr-6 pt-2 backdrop-blur supports-[backdrop-filter]:bg-background/60 border-b-[1px] border-b-border"
+    >
+      <ul
         data-orientation="horizontal"
+        className="flex list-none justify-between"
         dir="ltr"
-        className="flex max-w-max flex-1 items-center justify-center"
       >
-        <ul
-          data-orientation="horizontal"
-          className="group flex flex-1 list-none items-center justify-center space-x-1"
-          dir="ltr"
-        >
-          <li>
-            <Link
-              className="inline-flex h-10 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50"
-              href="/about"
-            >
-              About
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
+        <li className="flex items-center">
+          <Link
+            className=" inline-flex h-10 w-max items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50"
+            href="/"
+          >
+            <span className="text-lg">❀</span>사전
+          </Link>
+          <Link
+            className="inline-flex h-10 w-max items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50"
+            href="/about"
+          >
+            About
+          </Link>
+        </li>
+
+        <li>
+          <SajeonThemeSelector />
+        </li>
+      </ul>
+    </nav>
   );
 }
 
