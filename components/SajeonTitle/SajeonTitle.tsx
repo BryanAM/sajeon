3aeee0fdf99d954e28d379171aa2f96e150bd32b
Bryan Aument <bryaument@gmail.com>
2023-11-25 07:31:26 -0500

    small title fix

--- components/SajeonTitle/SajeonTitle.tsx
+++ components/SajeonTitle/SajeonTitle.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function SajeonTitle() {
   return (
-    <div className="text-shadow bg-gradient-to-t from-neutral-900 to-neutral-700 bg-clip-text pb-3 text-8xl font-extrabold text-transparent md:text-9xl">
+    <div className="text-shadow pb-3 text-8xl font-extrabold text-transparent md:text-9xl">
       Sajeon
     </div>
   );
