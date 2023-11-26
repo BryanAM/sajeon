89d97f4bceb694447ba1127cb2748002ead29044
Bryan Aument <bryaument@gmail.com>
2023-11-26 11:07:32 -0500

    finished Aim section on about us

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -17,11 +17,10 @@ export default function About() {
         <SajeonTitle text="Sajeon" className="inline-block" />
       </h1>
       <p>
-        Sajeon is a user centered Korean English diciontary built by people who
-        love to study language. We are building a dictionary that we want to
-        use.
+        Sajeon is a user centered Korean English dictionary built by people who
+        love to study language. We are building a dictionary from a language learner's point of view, and one that you will love.
       </p>
-      <h2> <SajeonTitle text="Aim" className="text-shadow mt-10 text-6xl md:text-6xl" /></h2>
+      <h2> <SajeonTitle text="Aim" className="text-shadow mt-12 text-6xl md:text-6xl" /></h2>
       <Accordion type="single" collapsible>
         <AccordionItem value="item-1">
           <AccordionTrigger>User Experience (UX)</AccordionTrigger>
@@ -30,12 +29,26 @@ export default function About() {
           </AccordionContent>
         </AccordionItem>
         <AccordionItem value="item-2">
-          <AccordionTrigger>Accessibility</AccordionTrigger>
+          <AccordionTrigger>Accessible</AccordionTrigger>
           <AccordionContent>
             Sajeon aims to make learning Korean accessible by adhereing to WACG standards. Sajeon and it's components are a composite of <Link className={   ` ${buttonVariants({ variant: "link" })} px-0 py-0 pt-0 pb-0 h-auto`  } href="https://www.radix-ui.com/primitives">Radix</Link>, <Link className={   ` ${buttonVariants({ variant: "link" })} px-0 py-0 pt-0 pb-0 h-auto`  } href="https://ui.shadcn.com">shadcn's re-usable components</Link> and custom components. 
           </AccordionContent>
         </AccordionItem>
+        <AccordionItem value="item-3">
+          <AccordionTrigger>Collaborative</AccordionTrigger>
+          <AccordionContent>
+            Behind the shiny UI lies our most difficult task - comprehensive & annotated data. As you may imagine, a dictionary annoted with parts of speech, accurate definitions, example sentences and so on is no small feat. We are working devising a way for interested parties to contribute to Sajeon.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-4">
+          <AccordionTrigger>Scalable</AccordionTrigger>
+          <AccordionContent>
+            Over time we aim to scale the scope of Sajeon to include new data and features to improve your Korean learning experience.
+          </AccordionContent>
+        </AccordionItem>
       </Accordion>
+      <h2> <SajeonTitle text="Meet The Team" className="text-shadow mt-12 text-6xl md:text-6xl" /></h2>
+
     </>
   );
 }
