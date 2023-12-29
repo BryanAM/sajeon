797bee694021580b23dd655e265ca9ee5e2501c4
Bryan Aument <bryaument@gmail.com>
2023-12-29 13:03:24 -0500

    removed unused imports

--- components/SajeonSearch/SajeonSearch.tsx
+++ components/SajeonSearch/SajeonSearch.tsx
@@ -1,7 +1,6 @@
 'use client';
 
-import React, { useEffect, useState }from "react";
-import { FormHTMLAttributes } from 'react';
+import React, { useState }from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
