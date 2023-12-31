a3179691cf257e1847cc382ed35f8ea6c00eaf71
Bryan Aument <bryaument@gmail.com>
2023-12-30 20:18:53 -0500

    added some tests to search component'

--- components/SajeonSearch/__tests__/index.test.jsx
+++ components/SajeonSearch/__tests__/index.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { fireEvent, getByText, queryByRole, render, screen, waitFor } from '@testing-library/react'
 import SajeonSearch from '../SajeonSearch';
 
 describe('Sajeon Title', () => {
@@ -8,5 +8,27 @@ describe('Sajeon Title', () => {
 
     const element = results.container.querySelector('#search');
     expect(element).toBeInTheDocument();
+  });
+
+  it('sets input value is props', () => {
+    const results = render(<SajeonSearch inputValue="search item"/>)
+
+    const input = results.container.querySelector('input');
+    expect(input.value).toBe('search item');
+  });
+
+  it('changes input when a user types into the field', async () => {
+    const mockFormAction = jest.fn();
+    const results = render(<SajeonSearch formAction={mockFormAction}/>)
+    const submitButton = getByText(results.container, 'Search');
+    const input = results.container.querySelector('input');
+
+    fireEvent.change(input, { target: { value: 'Study' } });
+    fireEvent.click(submitButton);
+
+    await waitFor(() => {
+      expect(input.value).toBe('Study');
+      expect(mockFormAction).toHaveBeenCalled();
+    });
   });
 });
