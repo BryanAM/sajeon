df6774618fd27ac32bd14719ca907fb3633c5777
Bryan Aument <bryaument@gmail.com>
2024-01-01 10:18:05 -0500

    added footer test

---
+++ components/SajeonFooter/__tests__/index.test.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import SajeonFooter from '../SajeonFooter';
+
+describe('Sajeon Footer', () => {
+  it('renders footer', () => {
+    render(<SajeonFooter />)
+    expect(screen.getByText(/© 2022 Sajeon - All Rights Reserved/)).toBeInTheDocument();
+  });
+});
