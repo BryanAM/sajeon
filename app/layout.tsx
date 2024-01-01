0313e8bf469e3455dbbab8fd94775769e059e927
Bryan Aument <bryaument@gmail.com>
2024-01-01 10:11:52 -0500

    fixed various height issues related to adding footer component

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,13 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   description:
     "Search for Korean and English words online with Sajeon, a sleek, responsive, and beautiful dictionary.",
-  keywords: ["korean dictionary", "korean english dictionary", "study korean", "learn korean", "korean words"],
+  keywords: [
+    "korean dictionary",
+    "korean english dictionary",
+    "study korean",
+    "learn korean",
+    "korean words",
+  ],
   metadataBase: new URL(`${process.env.BASE_URL}`),
   openGraph: {
     images: ["/api/og"],
@@ -28,8 +34,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning={true}>
-      <body className={`${inter.className}`}>
+    <html
+      className="min-h-full relative flex flex-col"
+      lang="en"
+      suppressHydrationWarning={true}
+    >
+      <body className={`sajeon-body grow mb-[112px] ${inter.className}`}>
         <div className="m-auto max-w-5xl">
           <ThemeProvider
             attribute="class"
@@ -38,10 +48,10 @@ export default function RootLayout({
             disableTransitionOnChange
           >
             <SajeonNavigation />
-            <div className="mt-[88px] p-6">{children}</div>
-            <SajeonFooter />
+            <div className="mt-[88px] p-6  min-h-[100svh]">{children}</div>
           </ThemeProvider>
         </div>
+        <SajeonFooter />
       </body>
     </html>
   );
--- components/SajeonFooter/SajeonFooter.tsx
+++ components/SajeonFooter/SajeonFooter.tsx
@@ -1,11 +1,16 @@
 import React from "react";
+import SajeonTitle from "../SajeonTitle/SajeonTitle";
 
 function SajeonFooter() {
   return (
-    <footer className="">
-      footer
+    <footer className="absolute inset-x-0 bottom-0 bg-muted p-6">
+      <div className="m-auto max-w-5xl text-muted-foreground">
+        <h2 className="pb-3 text-xl">❀ Sajeon</h2>
+        
+        <p>© 2022 Sajeon - All Rights Reserved</p>
+      </div>
     </footer>
-  )
+  );
 }
 
 export default SajeonFooter;
