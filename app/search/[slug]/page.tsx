b1bf51d4f048a9bf8a607cc07cd352a0b4dcc36c
Bryan Aument <bryaument@gmail.com>
2024-01-06 20:10:40 -0500

    added more mock data

--- app/search/[slug]/page.tsx
+++ app/search/[slug]/page.tsx
@@ -14,10 +14,9 @@ export default async function Search({ params }: { params: { slug: string } }) {
       />
       <section className="mt-4 max-w-xl m-auto">
         <h2 className="mb-4 text-2xl font-semibold">Results</h2>
-        <SajeonVocabCard data={dataMock[0]} />
-        <SajeonVocabCard data={dataMock[1]} />
-        <SajeonVocabCard data={dataMock[0]} />
-        <SajeonVocabCard data={dataMock[0]} />
+        {dataMock.map((data) => (
+           <SajeonVocabCard key={data.ID} data={data} />
+        ))}
       </section>
     </main>
   );
