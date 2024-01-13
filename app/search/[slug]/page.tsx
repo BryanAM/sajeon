c0bc382a48439347cbc528dabe21623c31354ce3
Bryan Aument <bryaument@gmail.com>
2024-01-13 14:03:23 -0500

    implemented offset pagination basics:

--- app/search/[slug]/page.tsx
+++ app/search/[slug]/page.tsx
@@ -7,17 +7,22 @@ import SajeonVocabCard from "@/components/SajeonVocabCard/SajeonVocabCard";
 import { dataMock } from "@/__mocks__/dataMock";
 import SajeonPagination from "@/components/SajeonPagination/SajeonPagination";
 
-
 type SearchProps = {
-  params: { slug: string }
-  searchParams: { [key: string]: string | string[] }
-}
+  params: { slug: string };
+  searchParams: { [key: string]: string | string[] };
+};
 export default async function Search({ params, searchParams }: SearchProps) {
-
   // pretend fetch to database
   const dataFetchResults = dataMock;
-  console.log(searchParams);
-  
+  function getOffset(): number {
+    // not on the first page
+    if (Object.hasOwn(searchParams, "page")) {
+      return +searchParams.page - 1;
+    } else {
+      // we are on the first page i.e. no paramater
+      return 0;
+    }
+  }
 
   return (
     <main>
@@ -25,12 +30,17 @@ export default async function Search({ params, searchParams }: SearchProps) {
         formAction={formAction}
         inputValue={decodeURIComponent(params.slug)}
       />
-      <section className="mt-4 max-w-xl m-auto">
+      <section className="m-auto mt-4 max-w-xl">
         <h2 className="mb-4 text-2xl font-semibold">Results</h2>
-        {dataFetchResults.slice(0, 10).map((data) => (
-           <SajeonVocabCard key={data.ID} data={data} />
-        ))}
-        <SajeonPagination currentPage={searchParams} pages={Math.ceil(dataFetchResults.length / 10)}/>
+        {dataFetchResults
+          .slice(getOffset() * 10, getOffset() * 10 + 10)
+          .map((data) => (
+            <SajeonVocabCard key={data.ID} data={data} />
+          ))}
+        <SajeonPagination
+          currentPage={searchParams}
+          pages={Math.ceil(dataFetchResults.length / 10)}
+        />
       </section>
     </main>
   );
