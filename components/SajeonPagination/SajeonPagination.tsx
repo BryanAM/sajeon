9a362d1f7bc967089e8917f2eba4d3f3d102b49a
Bryan Aument <bryaument@gmail.com>
2024-01-14 15:20:58 -0500

    added pagination functions

--- components/SajeonPagination/SajeonPagination.tsx
+++ components/SajeonPagination/SajeonPagination.tsx
@@ -19,6 +19,7 @@ function SajeonPagination({ pages, currentPage }: SajeonPaginationProps) {
 
   const isLastPage = () => +currentPage.page === pages;
   const isFirstPage = () => +currentPage.page === 1 || !+currentPage.page;
+  
 
   function getNextPage(): string {
     if(Object.hasOwn(currentPage, 'page')) {
@@ -38,11 +39,34 @@ function SajeonPagination({ pages, currentPage }: SajeonPaginationProps) {
     }
   }
 
+
+  function getAdjustedStartPage(): number {
+    if (pages <= 3) {
+      return 1;
+    }
+     // Calculate the total number of layers
+    const totalLayers = Math.ceil(pages / 3);
+    const currentLayerStart = 3 * getCurrentLevel() + 1;
+    const pageRemainder = Math.abs((+currentPage.page % 3) - 3);
+    const hangingPages = (getCurrentLevel() + 1) === totalLayers && pageRemainder > 0 ? pageRemainder : 0;
+    // return offset if we have any hanging pages
+    return  currentLayerStart - hangingPages
+  }
+
+  function getCurrentLevel(): number {
+    if (Object.hasOwn(currentPage, 'page')) {
+      const currPage: number = +currentPage.page;
+      return Math.floor((currPage - 1) / 3);
+    } else {
+      return 0;
+    }
+  }
+
   return (
     <Pagination>
       <PaginationContent>
         <PaginationPrevious className={cn(`${isFirstPage() && 'pointer-events-none opacity-[0.25]'}`)} href={`?page=${getPreviousPage()}`} aria-disabled={isFirstPage()} />
-        {Array.from({ length: pages }, (_, index: number) => index + 1).map(
+        {Array.from({ length: Math.min(pages, 3) }, (_, index: number) => getAdjustedStartPage() + index).map(
           (item) => (
             <PaginationLink key={item} href={`?page=${item}`} isActive={+currentPage.page === item || !+currentPage.page && item === 1}>{item}</PaginationLink>
           ),
