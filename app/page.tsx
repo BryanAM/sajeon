8a6a4e4fef8a446fe67d243ff4b837d8ff6860b7
Bryan Aument <bryaument@gmail.com>
2024-01-21 18:10:13 -0500

    small alignement upate on home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,11 @@ import { formAction } from "@/app/actions";
 
 export default function Home() {
   return (
-    <main>
+    <main className="text-left md:text-center">
       <h1 className="sajeon-branded-text text-center mb-4">Sajeon</h1>
       <SajeonSearch formAction={formAction}/>
       <h2 className="mb-2 mt-8 pb-3 text-4xl font-extrabold text-shadow-inverted md:text-5xl lg:text-6xl">A simple and easy to use online Korean English Dictionary.</h2>
-      <p className="text-xl">Get started by entering a word into the search bar and pressing the search button!</p>
+      <p className="text-lg">Get started by entering a word into the search bar and pressing the search button!</p>
     </main>
   )
 }
