74ea978d5e22318ba48a7a879fad5affa9f67e6a
Bryan Aument <bryaument@gmail.com>
2024-01-21 13:38:29 -0500

    fixed up tests

--- e2e/search.spec.ts
+++ e2e/search.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, } from '@playwright/test';
 
 test('should do nothing when search is empty and we attempt to search an item', async ({ page }) => {
   await page.goto('http://localhost:8080');
@@ -29,15 +29,23 @@ test('should load input when link is shared with endpoint 공부하다', async (
 });
 
 test('should load new input after link is shared with endpoint 공부하다 and searched to new endpoint "study"', async ({ page }) => {
-  await page.goto('http://localhost:8080/search/공부하다');
-  await expect(page.getByRole('searchbox')).toHaveValue('공부하다');
+  await page.goto('http://localhost:8080');
+  await expect(page.getByRole('searchbox')).toHaveValue('');
 
   // navigate to new page with new search
-  await page.getByRole('searchbox').fill('study');
-  await page.getByRole('button', { name: 'Search' }).click();
-  await expect(page).toHaveURL('http://localhost:8080/search/study')
+  await page.getByRole('searchbox').fill('test');
+  await expect(page.getByRole('searchbox')).toHaveValue('test')
+
+  await Promise.all([
+    page.getByTestId('search-desktop').waitFor(),
+    page.getByTestId('search-desktop').click({force: true})
+  ]);
+
+  await page.keyboard.press('Enter');
+
+  await expect(page).toHaveURL('http://localhost:8080/search/test')
 
   // input should also update
-  await expect(page.getByRole('searchbox')).toHaveValue('study')
+  await expect(page.getByRole('searchbox')).toHaveValue('test')
 
 });
--- models/Word.ts
+++ models/Word.ts
@@ -9,6 +9,18 @@ const WordSchema = new Schema({
   sentences: [Object],
 });
 
+const TestSchema = new Schema({
+  ID: Number,
+  word: String,
+  romaja: String,
+  hanja: String,
+  definitions: [String],
+  pos: String,
+  sentences: [Object],
+});
+
+const TestModel = mongoose.model('Test', TestSchema, 'testDB');
+
 // Creating a text index for all fields
 Object.keys(WordSchema.paths).forEach(function (path) {
   if (WordSchema.paths[path].instance === "String") {
@@ -16,4 +28,12 @@ Object.keys(WordSchema.paths).forEach(function (path) {
   }
 });
 
-export default mongoose.models.Word || mongoose.model("Word", WordSchema);
+const exportTestModule = mongoose.models.Test || TestModel;
+const exportProdModule = mongoose.models.Word || mongoose.model("Word", WordSchema);
+
+const modelToExport = process.env.NODE_ENV === "test" 
+  ? exportTestModule 
+  : exportProdModule;
+
+
+export default exportProdModule;
