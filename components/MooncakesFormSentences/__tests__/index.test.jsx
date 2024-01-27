2679106537d4b17a0a49a867474587f5a1605b2f
Bryan Aument <bryaument@gmail.com>
2024-01-27 13:17:09 -0500

    fixed last test for sentences

--- components/MooncakesFormSentences/__tests__/index.test.jsx
+++ components/MooncakesFormSentences/__tests__/index.test.jsx
@@ -186,24 +186,26 @@ describe('Mooncakes Form Sentences', () => {
     })
   })
 
-  xit('types in two new sentences then deletes second to last one', async () => {
+  it('types in two new sentences then deletes second to last one', async () => {
     render(<MooncakesFormSentences word={databaseDataMock[0]}/>)
     const addButtonTrigger = screen.getByRole('button', { name: 'Add Sentence'})
+    await userEvent.click(addButtonTrigger);
     await userEvent.click(addButtonTrigger);
 
     const sentences = screen.getAllByLabelText('Korean')
     // new sentence in last korean text box
     // this first one will bascially add to end of first, not delete it
-    await userEvent.type(sentences[0], '행복은 종종 작은 것에서 찾을 수 있습니다.');
-    await userEvent.type(sentences[sentences.length - 1], '행복은 종종.');
+    await userEvent.type(sentences[sentences.length - 1], '행복은 종종 작은 것에서 찾을 수.');
+    await userEvent.type(sentences[sentences.length - 2], '행복은 종종.');
 
     const deleteButtons = screen.getAllByLabelText('delete icon')
 
     // all but first sentence
     await userEvent.click(deleteButtons[deleteButtons.length - 2]);
+    const updatedSentences = screen.getAllByLabelText('Korean')
 
-    const validValues = ['그녀는 그에게 사랑을 고백했다.행복은 종종 작은 것에서 찾을 수 있습니다.', '사랑은 인생의 가장 강력한 감정 중 하나입니다.', '행복은 종종.'];
-    sentences.forEach((def) => {
+    const validValues = ['그녀는 그에게 사랑을 고백했다.', '사랑은 인생의 가장 강력한 감정 중 하나입니다.', '그들은 서로 사랑하는 마음을 키워나갔다.', '행복은 종종 작은 것에서 찾을 수.'];
+    updatedSentences.forEach((def) => {
       expect(validValues).toContain(def.value)
     })
   })
