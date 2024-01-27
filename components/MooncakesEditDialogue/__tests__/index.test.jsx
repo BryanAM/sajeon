72c2ec7fc4a1ec0a2cdbc0d81652e90ac9ca9ea1
Bryan Aument <bryaument@gmail.com>
2024-01-27 10:48:51 -0500

    updated edit dialogue tests

--- components/MooncakesEditDialogue/__tests__/index.test.jsx
+++ components/MooncakesEditDialogue/__tests__/index.test.jsx
@@ -25,5 +25,26 @@ describe('Mooncakes Edit Dialogue', () => {
     expect(title).toBeInTheDocument();
   });
 
+  it('dialogue renders data into fields  ', async () => {
+    render(<MoonCakesEditDialogue word={databaseDataMock[0]}/>)
+    const dialogueTrigger = screen.getByRole('button', { name: 'Edit Data'})
+    
+    await userEvent.click(dialogueTrigger);
 
+    const wordInput = screen.getByDisplayValue('사랑');
+    expect(wordInput.value).toBe('사랑')
+
+    const romajaInput = screen.getByDisplayValue('sarang');
+    expect(romajaInput.value).toBe('sarang');
+
+    const hanjaInput = screen.getByDisplayValue('愛');
+    expect(hanjaInput.value).toBe('愛');
+
+    const definitionInput = screen.getByDisplayValue('test');
+    expect(definitionInput.value).toBe('test');
+
+
+    const sentenceInput = screen.getByDisplayValue('그녀는 그에게 사랑을 고백했다.');
+    expect(sentenceInput.value).toBe('그녀는 그에게 사랑을 고백했다.');
+  });
 }); 
