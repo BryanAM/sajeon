ba1864e8fbbca98c29f2e1e2a70fc1263711044b
Bryan Aument <bryaument@gmail.com>
2024-08-14 12:31:00 +0900

    updated favicon and apple-touch-icons for saving sajeon on iPhone

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/ThemeProvider/ThemeProvider";
 import SajeonFooter from "@/components/SajeonFooter/SajeonFooter";
-import { Toaster } from "@/components/ui/sonner"
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,6 +27,67 @@ export const metadata: Metadata = {
     // shows large image on twitter
     card: "summary_large_image",
   },
+  appleWebApp: {
+    capable: true,
+    title: "Sajeon",
+    statusBarStyle: "black-translucent",
+    startupImage: `${process.env.BASE_URL}/apple-touch-icon-180x180.png`,
+  },
+  icons: [
+    {
+      rel: "apple-touch-icon",
+      type: "image/png",
+      url: `${process.env.BASE_URL}/apple-touch-icon.png`,
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "57x57",
+      type: "image/png",
+      url: `${process.env.BASE_URL}/apple-touch-icon-57x57.png`,
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "60x60",
+      type: "image/png",
+      url: `${process.env.BASE_URL}/apple-touch-icon-60x60.png`,
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "76x76",
+      type: "image/png",
+      url: `${process.env.BASE_URL}/apple-touch-icon-76x76.png`,
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "120x120",
+      type: "image/png",
+      url: `${process.env.BASE_URL}/apple-touch-icon-120x120.png`,
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "152x152",
+      type: "image/png",
+      url: `${process.env.BASE_URL}/apple-touch-icon-152x152.png`,
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "167x167",
+      type: "image/png",
+      url: `${process.env.BASE_URL}/apple-touch-icon-167x167.png`,
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      type: "image/png",
+      url: `${process.env.BASE_URL}/apple-touch-icon-180x180.png`,
+    },
+    {
+      rel: "icon",
+      url: `${process.env.BASE_URL}/favicon.ico`,
+      type: "image/x-icon",
+      sizes: "16x16",
+    },
+  ],
 };
 
 export default function RootLayout({
@@ -36,11 +97,11 @@ export default function RootLayout({
 }) {
   return (
     <html
-      className="min-h-full relative flex flex-col"
+      className="relative flex min-h-full flex-col"
       lang="en"
       suppressHydrationWarning={true}
     >
-      <body className={`sajeon-body grow mb-[112px] ${inter.className}`}>
+      <body className={`sajeon-body mb-[112px] grow ${inter.className}`}>
         <div className="m-auto max-w-5xl">
           <ThemeProvider
             attribute="class"
@@ -48,11 +109,8 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
-            
             <SajeonNavigation />
-            <div className="mt-[88px] p-6  min-h-[100svh]">
-            {children}
-            </div>
+            <div className="mt-[88px] min-h-[100svh]  p-6">{children}</div>
           </ThemeProvider>
         </div>
         <SajeonFooter />
