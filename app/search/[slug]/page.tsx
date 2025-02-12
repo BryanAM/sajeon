a4294f298bd8634be3da513729ecdf0bd8e43d61
Bryan Aument <bryaument@gmail.com>
2025-02-12 10:43:53 -0500

    added complex query to tokenize input by space

--- app/search/[slug]/page.tsx
+++ app/search/[slug]/page.tsx
@@ -28,14 +28,26 @@ async function getData(params: SearchProps["params"]) {
     }
 
     const cleanedQuery: string = safeQuery(decodedQuery);
-    const regex = new RegExp(cleanedQuery, "i");
-    const query = {
-      $or: [
-        { word: regex }, // Search in 'word' (Korean word)
-        { definitions: regex }, // Search in 'definitions' (English definition)
-        { romaja: regex }, // searched romanized korean
-      ],
-    };
+    const koreanCharsRegex = /[\uAC00-\uD7A3]/;
+    const tokens = cleanedQuery.split(/\s+/).filter(Boolean);
+
+    /*
+    each token will be search either in Korean or English 
+    e.g. 평화 book => [평화, book] where 평화 => { word: 평화 } 
+    {definitions: book} or { romaja: book}
+     */
+    const conditions = tokens.map((token) => {
+      const tokenRegex = new RegExp(token, "i");
+      return koreanCharsRegex.test(token)
+        ? { word: tokenRegex }
+        : { $or: [{ definitions: tokenRegex }, { romaja: tokenRegex }] };
+    });
+
+    /**
+     * If we have a compound allow results from each token so
+     * 평화 book will return restuls from either the first or second
+     */
+    const query = { $or: conditions };
 
     const words = await Word.find(query).limit(100).lean(); // Updated query
     return new Response(JSON.stringify(words), {
