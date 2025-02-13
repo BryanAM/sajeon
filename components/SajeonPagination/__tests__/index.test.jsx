1f60efb6228b5ac9fa7f76afad385d34df5613fa
Bryan Aument <bryaument@gmail.com>
2025-02-12 20:16:47 -0500

    updated tests for currentPage val

--- components/SajeonPagination/__tests__/index.test.jsx
+++ components/SajeonPagination/__tests__/index.test.jsx
@@ -4,27 +4,21 @@ import { render, fireEvent, waitFor } from "@testing-library/react";
 
 describe("Sajeon Pagination Basics", () => {
   it("renders the component", () => {
-    const results = render(
-      <SajeonPagination totalPages={5} currentPage={{ page: 1 }} />,
-    );
+    const results = render(<SajeonPagination totalPages={5} currentPage={1} />);
 
     const element = results.getByRole("navigation");
     expect(element).toBeInTheDocument();
   });
 
   it("First page is active by default", () => {
-    const results = render(
-      <SajeonPagination totalPages={5} currentPage={{ page: 1 }} />,
-    );
+    const results = render(<SajeonPagination totalPages={5} currentPage={1} />);
 
     const element = results.getByRole("link", { name: "1" });
     expect(element.getAttribute("aria-current")).toBe("page");
   });
 
   it("Visible links should be 1, 2, 3", () => {
-    const results = render(
-      <SajeonPagination totalPages={5} currentPage={{ page: 1 }} />,
-    );
+    const results = render(<SajeonPagination totalPages={5} currentPage={1} />);
 
     const links = results.getAllByRole("link").slice(1, 4);
     links.forEach((link, index) => {
@@ -33,9 +27,7 @@ describe("Sajeon Pagination Basics", () => {
   });
 
   it("Expect previous to be disabed on load", () => {
-    const results = render(
-      <SajeonPagination totalPages={5} currentPage={{ page: 1 }} />,
-    );
+    const results = render(<SajeonPagination totalPages={5} currentPage={1} />);
 
     const previousLink = results.getByRole("link", {
       name: "Go to previous page",
@@ -44,9 +36,7 @@ describe("Sajeon Pagination Basics", () => {
   });
 
   it("Expect next page to be enabled on load", () => {
-    const results = render(
-      <SajeonPagination totalPages={5} currentPage={{ page: 1 }} />,
-    );
+    const results = render(<SajeonPagination totalPages={5} currentPage={1} />);
 
     const nextLink = results.getByRole("link", { name: "Go to next page" });
     expect(nextLink.getAttribute("aria-disabled")).toBe("false");
@@ -55,45 +45,35 @@ describe("Sajeon Pagination Basics", () => {
 
 describe("Sajeon Pagination Ellipsis Testing", () => {
   it("One page, no ellipsis", () => {
-    const results = render(
-      <SajeonPagination totalPages={1} currentPage={{ page: 1 }} />,
-    );
+    const results = render(<SajeonPagination totalPages={1} currentPage={1} />);
 
     const span = results.queryByText("More totalPages", { hidden: true });
     expect(span).not.toBeInTheDocument();
   });
 
   it("Two totalPages, no ellipsis", () => {
-    const results = render(
-      <SajeonPagination totalPages={2} currentPage={{ page: 1 }} />,
-    );
+    const results = render(<SajeonPagination totalPages={2} currentPage={1} />);
 
     const span = results.queryByText("More totalPages", { hidden: true });
     expect(span).not.toBeInTheDocument();
   });
 
   it("Three totalPages, no ellipsis", () => {
-    const results = render(
-      <SajeonPagination totalPages={3} currentPage={{ page: 1 }} />,
-    );
+    const results = render(<SajeonPagination totalPages={3} currentPage={1} />);
 
     const span = results.queryByText("More totalPages", { hidden: true });
     expect(span).not.toBeInTheDocument();
   });
 
   it("Four totalPages, ellipsis found", () => {
-    const results = render(
-      <SajeonPagination totalPages={4} currentPage={{ page: 1 }} />,
-    );
+    const results = render(<SajeonPagination totalPages={4} currentPage={1} />);
 
     const span = results.queryByText("More totalPages", { hidden: true });
     expect(span).toBeInTheDocument();
   });
 
   it("Last page, ellipsis found i.e. because there are previous ones", () => {
-    const results = render(
-      <SajeonPagination totalPages={7} currentPage={{ page: 7 }} />,
-    );
+    const results = render(<SajeonPagination totalPages={7} currentPage={7} />);
 
     const span = results.queryByText("More totalPages", { hidden: true });
     expect(span).toBeInTheDocument();
