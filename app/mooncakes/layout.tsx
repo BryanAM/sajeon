80b04a11757388e54cabe4242b87b4e357c6ce70
Bryan Aument <bryaument@gmail.com>
2025-02-21 10:57:28 -0500

    deleted generate metadata from mooncakes layout, unnecessary because we aren't sharing anything here outside of admins or specfic users

--- app/mooncakes/layout.tsx
+++ app/mooncakes/layout.tsx
@@ -6,25 +6,6 @@ type LayoutProps = {
   children: React.ReactNode;
 };
 
-export async function generateMetadata({
-  params,
-}: {
-  params: { slug: string };
-}) {
-  return {
-    title: `Korean Dictionary Search Results For "${decodeURIComponent(
-      params.slug,
-    )}"`,
-    openGraph: {
-      images: [`/api/og?description=Search Results For - ${params.slug}`],
-    },
-  };
-}
-
 export default function RootLayout({ children, params }: LayoutProps) {
-  return (
-    <div>
-      {children}
-    </div>
-  );
+  return <div>{children}</div>;
 }
