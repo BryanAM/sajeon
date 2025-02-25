677239e85ea7c4751d87dc616d5e7f7a16b36f47
Bryan Aument <bryaument@gmail.com>
2025-02-25 12:08:25 -0500

    removed nextauth and package

---
+++ app/search/[slug]/details/page.tsx
@@ -0,0 +1,10 @@
+export default function Details() {
+  return (
+    <>
+      <h1 className="mb-2 pb-3 text-7xl font-extrabold text-shadow-inverted md:text-9xl">
+        Studying Korean made simple{" "}
+        <span className="sajeon-branded-text">Sajeon</span>
+      </h1>
+    </>
+  );
+}
--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,12 @@
-export { default } from "next-auth/middleware";
+import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
 
-export const config = { matcher: ["/mooncakes/:path*"] };
+export default withAuth(async function middleware(req) {}, {
+  // Middleware still runs on all routes, but doesn't protect the blog route
+  publicPaths: ["/search", "/about", "/"],
+});
+
+export const config = {
+  matcher: [
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+  ],
+};
