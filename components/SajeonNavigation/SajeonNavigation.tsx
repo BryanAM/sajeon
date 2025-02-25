1ca5704ceae51c05a4a9780f753099e0cf0d52f3
Bryan Aument <bryaument@gmail.com>
2025-02-25 10:43:50 -0500

    fixed signin link styles to match nav

--- components/SajeonNavigation/SajeonNavigation.tsx
+++ components/SajeonNavigation/SajeonNavigation.tsx
@@ -31,9 +31,9 @@ function SajeonNavigation() {
           >
             About
           </Link>
-          <span className="inline-flex h-10 w-max items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50">
-            <LoginLink>Sign in</LoginLink>
-          </span>
+          <LoginLink className="inline-flex h-10 w-max items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50">
+            Sign in
+          </LoginLink>
         </li>
 
         <li>
