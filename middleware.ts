2c9475921db445c9b51ee8e2a4cec5e30093dd16
Bryan Aument <bryaument@gmail.com>
2025-02-26 12:05:26 -0500

    improved middleware to check for permissions before allowing routing to mooncakes

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,20 @@
 import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
+import type { NextRequest } from "next/server";
 
-export default withAuth(async function middleware(req) {}, {
-  // Middleware still runs on all routes, but doesn't protect the blog route
+export default withAuth(async function middleware(req: NextRequest) {}, {
+  // Middleware will allow non-authenticated users of the following routes
   publicPaths: ["/search", "/about", "/"],
+  isAuthorized: ({ token, req }: { token: any; req: NextRequest }) => {
+    const path = req.nextUrl.pathname;
+    if (path === "/mooncakes") {
+      const requiredPermissions = new Set(["view:mooncakes", "edit:mooncakes"]);
+      return token.permissions.some((permission: string) =>
+        requiredPermissions.has(permission),
+      );
+    }
+
+    return true;
+  },
 });
 
 export const config = {
