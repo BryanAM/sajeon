1945d84eb646488ca181e46b74a8cb7d7358a957
Bryan Aument <bryaument@gmail.com>
2025-02-28 18:05:15 -0500

    added mooncakes link for auth user

--- components/SajeonNavigation/SajeonNavigation.tsx
+++ components/SajeonNavigation/SajeonNavigation.tsx
@@ -52,13 +52,25 @@ async function SajeonNavigation() {
                 About
               </Link>
             </li>
-            <li>
-              {isLoggedIn && (
+
+            {isLoggedIn && (
+              <li>
+                <Link
+                  className="inline-flex h-10 w-max items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50"
+                  href="/mooncakes"
+                >
+                  Mooncakes
+                </Link>
+              </li>
+            )}
+
+            {isLoggedIn && (
+              <li>
                 <LogoutLink className="inline-flex h-10 w-max items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50">
                   Logout
                 </LogoutLink>
-              )}
-            </li>
+              </li>
+            )}
           </ul>
         </li>
 
