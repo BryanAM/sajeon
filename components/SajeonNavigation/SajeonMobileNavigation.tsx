6c08d12218df93a91f691029304191fd8269eaa7
Bryan Aument <bryaument@gmail.com>
2025-03-12 22:03:37 -0400

    added close function for each button - mobile

--- components/SajeonNavigation/SajeonMobileNavigation.tsx
+++ components/SajeonNavigation/SajeonMobileNavigation.tsx
@@ -58,20 +58,24 @@ export const SajeonMobileNavigation = ({
               className="flex list-none flex-col justify-between"
             >
               <li>
-                <Link
-                  className=" inline-flex h-10 w-max items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50"
-                  href="/"
-                >
-                  <span className="text-lg"></span>Home
-                </Link>
+                <SheetClose asChild>
+                  <Link
+                    className=" inline-flex h-10 w-max items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50"
+                    href="/"
+                  >
+                    <span className="text-lg"></span>Home
+                  </Link>
+                </SheetClose>
               </li>
               <li>
-                <Link
-                  className="inline-flex h-10 w-max items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50"
-                  href="/about"
-                >
-                  About
-                </Link>
+                <SheetClose asChild>
+                  <Link
+                    className="inline-flex h-10 w-max items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50"
+                    href="/about"
+                  >
+                    About
+                  </Link>
+                </SheetClose>
               </li>
 
               {userRole &&
@@ -80,18 +84,22 @@ export const SajeonMobileNavigation = ({
                   MOONCAKE_PERMISSIONS.view,
                 ]) && (
                   <li>
-                    <Link
-                      className="inline-flex h-10 w-max items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50"
-                      href="/mooncakes"
-                    >
-                      Mooncakes
-                    </Link>
+                    <SheetClose asChild>
+                      <Link
+                        className="inline-flex h-10 w-max items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50"
+                        href="/mooncakes"
+                      >
+                        Mooncakes
+                      </Link>
+                    </SheetClose>
                   </li>
                 )}
               {isAuthenticated && (
-                <li>
-                  <SajeonAuthButtons className="inline-flex h-10 w-max items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50" />
-                </li>
+                <SheetClose asChild>
+                  <li>
+                    <SajeonAuthButtons className="inline-flex h-10 w-max items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50" />
+                  </li>
+                </SheetClose>
               )}
             </ul>
           </nav>
