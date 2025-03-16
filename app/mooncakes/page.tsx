428354dcc6cf8b28adf56bd3877a748336c77b9f
Bryan Aument <bryaument@gmail.com>
2025-03-16 15:33:39 -0400

    fixed up the intro text and style

--- app/mooncakes/page.tsx
+++ app/mooncakes/page.tsx
@@ -54,12 +54,17 @@ export default async function MoonCakes({ searchParams }: SearchProps) {
 
   return (
     <main className="flex flex-col">
-      <h1 className="sajeon-branded-text  mb-4 text-center text-5xl md:text-8xl lg:text-9xl">
-        Mooncakes Editor
-      </h1>
-      <h2 className="mb-6 text-center text-xl font-extrabold text-shadow-inverted md:text-5xl lg:text-2xl">
-        Contributing to make Sajeon better.
-      </h2>
+      <div className="grid grid-cols-4">
+        <h1 className="col-span-4 mb-4 text-4xl font-extrabold text-shadow-inverted md:col-span-4">
+          Contributing to Sajeon
+        </h1>
+        <h2 className="text-md col-span-4 mb-6 text-muted-foreground md:col-span-3 md:text-lg">
+          Welcome to the mooncakes editor. Here, you can search, add, edit, and
+          manage words in our Korean-English dictionary, helping to refine and
+          expand our database. Your contributions make this resource more
+          accurate and valuable for language learners and enthusiasts alike.
+        </h2>
+      </div>
 
       <ol className="mb-6 grid grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-3">
         {dataFetchResults
