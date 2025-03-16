27ae29c158701dc8ee81de083753fc377e7e9f7b
Bryan Aument <bryaument@gmail.com>
2025-03-16 14:33:02 -0400

    updated details for checkPermissions function

--- app/api/auth/auth-utils.ts
+++ app/api/auth/auth-utils.ts
@@ -5,6 +5,8 @@ import { KindeAccessToken } from "@kinde-oss/kinde-auth-nextjs/types";
  *
  * Description: Given a user's token and set of valid premissions
  * return a boolean value whether or not a user has access
+ * RBAC - Role Based Access Control
+ *
  */
 export const checkPermissions = (
   token: KindeAccessToken | undefined,
