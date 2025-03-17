5575860c5d538143d9d4c6152a8c9d655e40969f
Bryan Aument <bryaument@gmail.com>
2025-03-16 22:16:26 -0400

    tweeks to styling on mooncakes-nav

--- app/mooncakes/mooncakes-navigation.tsx
+++ app/mooncakes/mooncakes-navigation.tsx
@@ -11,19 +11,19 @@ export const MooncakesNavigation = () => {
       <ul className="flex gap-2 py-4">
         <li
           className={`p-1 text-sm transition-all duration-500 ease-in-out 
-            ${pathname === "/mooncakes" ? "scale-105 rounded-sm bg-muted font-bold opacity-100" : "scale-100 opacity-80"}`}
+            ${pathname === "/mooncakes" ? "scale-105 rounded-sm bg-muted font-semibold opacity-100" : "scale-100 opacity-80"}`}
         >
           <Link href="/mooncakes">Dashboard</Link>
         </li>
         <li
           className={`p-1 text-sm transition-all duration-500 ease-in-out 
-            ${pathname === "/mooncakes/edit" ? "scale-105 rounded-sm bg-muted font-bold opacity-100" : "scale-100 opacity-80"}`}
+            ${pathname === "/mooncakes/edit" ? "scale-105 rounded-sm bg-muted font-semibold opacity-100" : "scale-100 opacity-80"}`}
         >
           <Link href="/mooncakes/edit">Edit Words</Link>
         </li>
         <li
           className={`p-1 text-sm transition-all duration-500 ease-in-out 
-            ${pathname === "/mooncakes/feedback" ? "scale-105 rounded-sm bg-muted font-bold opacity-100" : "scale-100 opacity-80"}`}
+            ${pathname === "/mooncakes/feedback" ? "scale-105 rounded-sm bg-muted font-semibold opacity-100" : "scale-100 opacity-80"}`}
         >
           <Link href="/mooncakes/feedback">Feedback</Link>
         </li>
