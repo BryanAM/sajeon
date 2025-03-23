59719bb4df1f712a65d98de2ec9dfd77012c063b
Bryan Aument <bryaument@gmail.com>
2025-03-22 20:55:50 -0400

    added basic table from components

--- app/mooncakes/edit/page.tsx
+++ app/mooncakes/edit/page.tsx
@@ -1,3 +1,26 @@
+import { MooncakesTable } from "@/components/MooncakesTable/MooncakesTable";
+import { columns } from "./colums";
+
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    // ...
+  ];
+}
+
 export default async function MooncakesEdit() {
-  return <main>edit page</main>;
+  const data = await getData();
+
+  return (
+    <main>
+      <h2>Header And Other Informations</h2>
+      <MooncakesTable columns={columns} data={data} />
+    </main>
+  );
 }
