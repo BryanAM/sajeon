9564c108386970f15242ba6723154045dd772400
Bryan Aument <bryaument@gmail.com>
2025-03-23 21:15:24 -0400

    updating column defs for table

--- app/mooncakes/edit/colums.tsx
+++ app/mooncakes/edit/colums.tsx
@@ -1,36 +1,43 @@
 "use client";
 
-import { ColumnDef } from "@tanstack/react-table";
-
+import { ColumnDef, createColumnHelper } from "@tanstack/react-table";
+import { DictionaryEntryType } from "@/types/SajeonTypes";
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
-export type Payment = {
-  id: string;
-  amount: number;
-  status: "pending" | "processing" | "success" | "failed";
-  email: string;
-};
 
-export const columns: ColumnDef<Payment>[] = [
-  {
-    accessorKey: "status",
-    header: "Status",
-  },
-  {
-    accessorKey: "email",
-    header: "Email",
-  },
-  {
-    accessorKey: "amount",
-    header: "Amount",
-    cell: ({ row }) => {
-      const amount = parseFloat(row.getValue("amount"));
-      const formatted = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD",
-      }).format(amount);
+const columnHelper = createColumnHelper<DictionaryEntryType>();
 
-      return <div className="text-right font-medium">{formatted}</div>;
-    },
-  },
+export const columns: ColumnDef<DictionaryEntryType>[] = [
+  columnHelper.display({
+    id: "word",
+    header: "Korean",
+  }),
+  columnHelper.display({
+    id: "romaja",
+    header: "Romaja",
+  }),
+  columnHelper.display({
+    id: "hanja",
+    header: "Hanja",
+  }),
+  columnHelper.display({
+    id: "pos",
+    header: "Part of Speech",
+  }),
+  columnHelper.display({
+    id: "sentences",
+    header: "Sentences",
+  }),
+  columnHelper.display({
+    id: "topik_level",
+    header: "Topik Level",
+  }),
+  columnHelper.display({
+    id: "frequency_score",
+    header: "Frequency Score",
+  }),
+  columnHelper.display({
+    id: "validation",
+    header: "Validation",
+  }),
 ];
