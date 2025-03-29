03024dfc1837232785e7f3c71cba73cccf525d6e
Bryan Aument <bryaument@gmail.com>
2025-03-29 10:35:24 -0400

    updated column and table data to accept the current sajeon data structure and display on edit words page

--- app/mooncakes/edit/colums.tsx
+++ app/mooncakes/edit/colums.tsx
@@ -1,39 +1,55 @@
 "use client";
 
-import { ColumnDef, createColumnHelper } from "@tanstack/react-table";
+import { createColumnHelper } from "@tanstack/react-table";
 import { DictionaryEntryType } from "@/types/SajeonTypes";
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
 
 const columnHelper = createColumnHelper<DictionaryEntryType>();
 
-export const columns: ColumnDef<DictionaryEntryType>[] = [
-  {
-    accessorKey: "word",
+export const columns = [
+  columnHelper.accessor("word", {
     header: "Korean",
-  },
-  {
-    accessorKey: "romaja",
-    header: "Romaja",
-  },
-  {
-    accessorKey: "hanja",
-    header: "Hanja",
-  },
-  {
-    accessorKey: "pos",
-    header: "Part of Speech",
-  },
-  {
-    accessorKey: "definitions",
+    cell: (info) => (
+      <span style={{ textWrap: "nowrap" }}>{info.getValue()}</span>
+    ),
+  }),
+  columnHelper.accessor("romaja", { header: "Romaja" }),
+  columnHelper.accessor("hanja", { header: "Hanja" }),
+  columnHelper.accessor("pos", { header: "Part of Speech" }),
+  columnHelper.accessor("definitions", {
     header: "Definitions",
-  },
-  {
-    accessorKey: "sentences",
+    cell: (info) => {
+      const defs = info.getValue();
+      return (
+        <span className="line-clamp-2 overflow-hidden text-ellipsis">
+          {defs.join(", ")}
+        </span>
+      );
+    },
+  }),
+  columnHelper.accessor("sentences", {
     header: "Sentences",
-  },
-  {
-    accessorKey: "explanation",
-    header: "explanation",
-  },
+    cell: (info) => {
+      const sentences = info.getValue();
+      return (
+        <div className="relative max-h-[6rem] overflow-hidden overflow-ellipsis">
+          {sentences.map((sentence, i) => (
+            <span key={i}>
+              <span className="block">{sentence.kr}</span>
+              <span className="block text-sm text-gray-500">{sentence.en}</span>
+            </span>
+          ))}
+        </div>
+      );
+    },
+  }),
+  columnHelper.accessor("explanation", {
+    header: "Explanation",
+    cell: (info) => (
+      <span className="line-clamp-2 overflow-hidden text-ellipsis">
+        {info.getValue()}
+      </span>
+    ),
+  }),
 ];
--- components/MooncakesTable/MooncakesTable.tsx
+++ components/MooncakesTable/MooncakesTable.tsx
@@ -17,7 +17,7 @@ import {
 } from "@/components/ui/table";
 
 interface DataTableProps<TData, TValue> {
-  columns: ColumnDef<TData, TValue>[];
+  columns: ColumnDef<TData, any>[];
   data: TData[];
 }
 
@@ -65,7 +65,7 @@ export function MooncakesTable<TData, TValue>({
                 data-state={row.getIsSelected() && "selected"}
               >
                 {row.getVisibleCells().map((cell) => (
-                  <TableCell key={cell.id}>
+                  <TableCell className="" key={cell.id}>
                     {flexRender(cell.column.columnDef.cell, cell.getContext())}
                   </TableCell>
                 ))}
